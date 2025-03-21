dba6b773b75e4a32002d2df0b96d52c0e52e56a6
koen-sikkema <koensikkema@hotmail.com>
2025-03-21 13:41:12 +0100

    [UPDATE] removed redundant code
    Removed redundant code.

--- app/services/wound_image_database_service.py
+++ app/services/wound_image_database_service.py
@@ -1,22 +1,19 @@
 from sqlalchemy.orm     import Session
 from fastapi            import FastAPI, Depends
 from sqlalchemy.orm     import Session
-from core.database      import get_database
+from core.database      import get_database, close_database
 from core.models        import WoundImage
 
 app = FastAPI()
 
-# Dependency voor de database
-def get_db():
-    db = get_database()
-    try:
-        yield db
-    finally:
-        db.close()
 
 @app.get("/images/{image_id}")
-async def get_wound_image(image_id: int, db: Session = Depends(get_db)):
+async def get_wound_image(image_id: int, db: Session = Depends(get_database)):
+    '''
+        This function returns the wound image with the given image_id.'
+    '''
     image = db.query(WoundImage).filter(WoundImage.image_id == image_id).first()
     if image:
         return image
+    Depends(close_database)
     return {"message": "Image not found"}
--- app/services/wound_image_metadata_database_service.py
+++ app/services/wound_image_metadata_database_service.py
@@ -1,22 +1,16 @@
 from sqlalchemy.orm     import Session
 from fastapi            import FastAPI, Depends
 from sqlalchemy.orm     import Session
-from core.database      import get_database
+from core.database      import get_database, close_database
 from core.models        import WoundImage
 
 app = FastAPI()
 
-# Dependency voor de database
-def get_db():
-    db = get_database()
-    try:
-        yield db
-    finally:
-        db.close()
 
 @app.get("/metadata/{metadata_id}")
-async def get_wound_image_metadata(metadata_id: int, db: Session = Depends(get_db)):
+async def get_wound_image_metadata(metadata_id: int, db: Session = Depends(get_database)):
     metadata = db.query(WoundImage).filter(WoundImage.metadata_id == metadata_id).first()
     if metadata:
         return metadata
+    Depends(close_database)
     return {"message": "Metadata not found"}
