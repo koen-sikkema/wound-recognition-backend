6f064117136dc2bf5b0146e56e67ebf8e77be15f
koen-sikkema <koensikkema@hotmail.com>
2025-03-25 16:47:50 +0100

    [HELP] mousepad broke in the middle of working
    i do not have the power to continue now

--- app/services/database_service.py
+++ app/services/database_service.py
@@ -1,31 +1,22 @@
-from sqlalchemy.orm      import Session
-from app.core.database   import SessionLocal
-from app.core.models     import WoundImage   
-
+from sqlalchemy.orm import Session
+from app.core.database import SessionLocal, get_database, close_database
+from app.core.models import WoundImage
 
 class DatabaseService:
-    _session = None
 
-    @classmethod
-    def get_db(cls) -> Session:
-        if cls._session is None:
-            cls._session = SessionLocal()
-        return cls._session
-
-    @classmethod
-    def close(cls):
-        if cls._session:
-            cls._session.close()
-            cls._session = None
-    
-    def save_image(self, image_data: dict):
+    async def save_image(self, image_data: dict):
         ''' 
         This function saves an image to the database.
         '''
-        db = self.get_db()
-        db_image = WoundImage(**image_data)
-        db.add(db_image)
-        db.commit()
-        db.refresh(db_image)
-        return db_image
-    
+        await get_database()  # Open de databaseverbinding
+        db = SessionLocal()
+        try:
+            db_image = WoundImage(**image_data)
+            db.add(db_image)
+            db.commit()
+            db.refresh(db_image)
+            return db_image
+        finally:
+            db.close()  # Sluit de sessie
+            await close_database()  # Sluit de databaseverbinding
+
