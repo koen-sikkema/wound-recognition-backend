bed4a9678b5254a7eff65b45c96bffb212417228
koen-sikkema <koensikkema@hotmail.com>
2025-04-08 11:35:51 +0200

    env stuk

--- wrb/Scripts/activate.bat
+++ wrb/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\koenw\Documents\Stage\wound-recognition-backend\wrb
+set VIRTUAL_ENV=C:\Users\koenw\Documents\Stage\wrb
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -17,8 +17,6 @@ if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
 set PROMPT=(wrb) %PROMPT%
-set PATH=wrb\Scripts;%PATH%
-
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
